74406d5c3078a9dd46aa73017fad6da1c2bff2d5
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-11 23:32:21 +0100

    added the grids

--- javascript.js
+++ javascript.js
@@ -1,28 +1,21 @@
 //Create a webpage with a 16x16 grid of square divs.
-
-let container = document.getElementById("container")
-
-
 //Create the divs using JavaScript. Don’t try to create them by hand by copying and pasting them in your HTML file!
-
-
-
-
 //It’s best to put your grid squares inside a “container” div. This div can be written in your HTML file.
 
+//creating the rows and columns with loops
+let container = document.getElementById("container")
 
+for (let i = 0; i < 16; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "rows")
+    container.appendChild(newDiv)
 
-/*Use Flexbox to make the divs appear as a grid (versus just one on each line). Despite the name,
- do not be tempted to research or use CSS Grid, as it will be taught in a later lesson after the foundations path.
-  This project is an opportunity specifically to practice Flexbox!
-Be careful with borders and margins, as they can adjust the size of the squares!
-“OMG, why isn’t my grid being created???”
-Did you link your CSS stylesheet?
-Open your browser’s developer tools.
-Check if there are any errors in the JavaScript console.
-Check your “elements” panel to see if the elements have actually shown up but are somehow hidden.
-Go willy-nilly and add console.log statements in your JavaScript to see if it’s actually being loaded.*/
-
+    for (let n = 0; n < 16; n++) {
+        let items = document.createElement("div");
+        items.setAttribute("class", "columns")
+        newDiv.appendChild(items)
+    }
+}
 
 
 /*Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through
