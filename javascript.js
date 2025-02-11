601933e98dd6ac4eb6730aeae34261377a5c43cf
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-11 23:47:54 +0100

    added hover effect

--- javascript.js
+++ javascript.js
@@ -13,21 +13,23 @@ for (let i = 0; i < 16; i++) {
     for (let n = 0; n < 16; n++) {
         let items = document.createElement("div");
         items.setAttribute("class", "columns")
+        items.addEventListener("mouseover", () => items.style.backgroundColor = "red")
         newDiv.appendChild(items)
+        
     }
 }
-
 
 /*Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through
  your grid like a pen would.
 Hint: “Hovering” is what happens when your mouse enters a div and ends when your mouse leaves it.
  You can set up event listeners for either of those events as a starting point.*/
 
-
-
 /*There are multiple ways to change the color of the divs, including:
 Adding a new class to the div.
 Changing the div’s background color using JavaScript.*/
+
+
+
 
 
 /*
