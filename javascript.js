70ecb7e2ac5f14215ba5050d3134776afa52d61f
FaridMo99 <farid-ahmad@hotmail.de>
2025-02-12 00:38:23 +0100

    added random background colors

--- javascript.js
+++ javascript.js
@@ -1,8 +1,21 @@
+// loops to create grid and changign the background color
+
 let container = document.getElementById("container")
 let body = document.querySelector("body")
 let button = document.createElement("button")
 
 body.insertBefore(button, container)
+
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 
 function creatingGrid(a, b){
 container.innerHTML = ""
@@ -14,7 +27,7 @@ for (let i = 0; i < a; i++) {
     for (let n = 0; n < b; n++) {
         let items = document.createElement("div");
         items.setAttribute("class", "columns")
-        items.addEventListener("mouseover", () => items.style.backgroundColor = "yellow")
+        items.addEventListener("mouseover", () => items.style.backgroundColor = getRandomColor())
         newDiv.appendChild(items)
         
     }
@@ -23,7 +36,7 @@ for (let i = 0; i < a; i++) {
 
 creatingGrid(16, 16);
 
-
+//button to change the grid size
 button.textContent = "Change Grid Size"
 button.addEventListener("click", () => {
     let input = prompt("What Size should your new Grid have?(Max:100)")
@@ -42,13 +55,3 @@ button.addEventListener("click", () => {
         alert("This is not a number")
     }
 })
-
-
-
-/*
-Extra credit
-Rather than squares being the same color throughout the grid, randomize the squares’ RGB values with each interaction.
-Additionally, implement a progressive darkening effect where each interaction darkens the square by 10%.
- The goal is to achieve a fully black (or completely colored) square in only ten interactions.
-Hint: The opacity CSS property is useful here. To learn how to use it, check this MDN docs article about the opacity CSS property.
-You can choose to do either one or both of these challenges, it’s up to you. */
